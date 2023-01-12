164e30c35bbb3f7791c38fcc871274e33a23768d
tj-bu-11 <tj-bu-11@DESKTOP-7GB30DL>
2023-01-12 14:54:48 +0900

    오류 수정중

--- src/NewTest/TestFrm4.java
+++ src/NewTest/TestFrm4.java
@@ -1,7 +1,7 @@
 package NewTest;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,6 +14,7 @@ import javax.swing.JScrollPane;
 public class TestFrm4 extends JFrame {
 	private JButton enqubt, inbt, outbt, listbt, setbt;
 	private JButton [] subBtn = new JButton[25];
+//	private JPanel pNorth, pSouth, subMenuContainer;
 	private JPanel pNorth, pSouth, subMenuContainer;
 	private JScrollPane pCenter;
 	private JButton[] btn = null;
@@ -31,50 +32,25 @@ public class TestFrm4 extends JFrame {
 		listbt.setBackground(Color.CYAN);
 		setbt = new JButton("설정");
 		setbt.setBackground(Color.CYAN);
-		
-		
-		
 		pNorth = new JPanel();
-		pNorth.setLayout(new GridLayout(5, 1));
-		
-		
-		pSouth = new JPanel();
-//		pSouth.setLayout(new GridLayout(2, 1));
-		
-		
 		subMenuContainer = new JPanel();
 		subMenuContainer.setLayout(new GridLayout(25, 1));
-		btn = new JButton[25];
-		for (int i = 0; i < btn.length; i++) {
-			btn[i] = new JButton("[미니 메뉴" + i + "]");
-			btn[i].setBackground(Color.WHITE);
-		}
-//		subBtn = new JButton[25];
-		for (int i = 0; i < subBtn.length; i++) {
+		for (int i = 0; i < 2; i++) {
 			subBtn[i] = new JButton("[미니 메뉴" + i + "]");
 			subBtn[i].setBackground(Color.WHITE);
 		}
 		
-		
-		this.setLayout(new BorderLayout());
-
+		pNorth.setLayout(new GridLayout(5, 1));
 		pNorth.add(enqubt);
 		pNorth.add(inbt);
 		pNorth.add(outbt);
 		pNorth.add(listbt);
 		pNorth.add(setbt);
-//		for (int i = 0; i < btn.length; i++) {
-//			subMenuContainer.add(btn[i]);
-//		}
-//		pCenter = new JScrollPane(subMenuContainer);
-//		pSouth.add(listbt);
-//		pSouth.add(setbt);
-		this.add(pNorth, "North");
-
-//		this.add(pCenter, "Center");
-//
-//		this.add(pSouth, "South");
-
+//		pNorth.setLocation(10,10);
+//		pNorth.setSize(300,400);
+		
+//		this.setLayout(null);
+		this.add(new JScrollPane(pNorth));
 		this.setVisible(true);
 		this.setSize(500, 600);
 		this.setResizable(false); // 창크기 조절 가능 여부
@@ -88,41 +64,24 @@ public class TestFrm4 extends JFrame {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			if (outbt == e.getSource()) {
-//				for (int i = 0; i < btn.length; i++) {
-//					subMenuContainer.add(btn[i]);
-//				}
 				if (expand) {// 펼침
 					pNorth.setLayout(new GridLayout(31, 1));
 					pNorth.remove(listbt);
 					pNorth.remove(setbt);
-					for (int i = 0; i < subBtn.length; i++) {
+					for (int i = 0; i < 2; i++) {
 						pNorth.add(subBtn[i]);
 					}
-//					pNorth.add(new JScrollPane(subMenuContainer));
-//					pCenter = new JScrollPane(subMenuContainer);
-//					getContentPane().add(pNorth, "Center");
 					pNorth.add(listbt);
 					pNorth.add(setbt);
-//					getContentPane().add(pSouth, "South");
-//					pSouth.setLayout(new GridLayout(2, 1));
 					validate();
 					getContentPane().repaint();
 					expand = false;
 				} else {// 접힘
-//					for (int i = 0; i < btn.length; i++) {
-//						subMenuContainer.remove(btn[i]);
-//					}
-					for (int i = 0; i < subBtn.length; i++) {
+					for (int i = 0; i < 2; i++) {
 						pNorth.remove(subBtn[i]);
 					}
-					pSouth.removeAll();
 					pNorth.setLayout(new GridLayout(5, 1));
-//					pNorth.add(listbt);
-//					pNorth.add(setbt);
 					pNorth.repaint();
-					pNorth1.repaint();
-//					pCenter.repaint();
-//					pSouth.repaint();
 					validate();
 					getContentPane().repaint();
 					expand = true;
