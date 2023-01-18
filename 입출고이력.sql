fec946edf1c13f8a35aaca725a79fa045e18de6a
tj-bu-11 <tj-bu-11@DESKTOP-7GB30DL>
2023-01-18 18:20:06 +0900

    sql 업로드

---
+++ 입출고이력.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM myproject.iohistory
+LIMIT 0, 1000
+
+-- Date: 2023-01-18 18:17
+*/
+INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`,`ordernum`,`oder_kind`,`work_date`,`ex_num`,`realnum`,`worker_id`,`complete`) VALUES ('k123','SK II ','화장품','IO20230117170516','입고',NULL,23,NULL,NULL,'yet');
+INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`,`ordernum`,`oder_kind`,`work_date`,`ex_num`,`realnum`,`worker_id`,`complete`) VALUES ('k123','SK II ','화장품','IO20230117171004','입고',NULL,40,NULL,NULL,'yet');
+INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`,`ordernum`,`oder_kind`,`work_date`,`ex_num`,`realnum`,`worker_id`,`complete`) VALUES ('k124','선크림','화장품','IO20230117171150','입고',NULL,60,NULL,NULL,'yet');
+INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`,`ordernum`,`oder_kind`,`work_date`,`ex_num`,`realnum`,`worker_id`,`complete`) VALUES ('k123','SK II ','화장품','IO20230117171357','입고',NULL,10,NULL,NULL,'yet');
+INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`,`ordernum`,`oder_kind`,`work_date`,`ex_num`,`realnum`,`worker_id`,`complete`) VALUES ('k123','SK II ','화장품','IO20230117171911','입고',NULL,33,NULL,NULL,'yet');
---
+++ 제품리스트.sql
@@ -0,0 +1,15 @@
+/*
+-- Query: SELECT * FROM myproject.productlist
+LIMIT 0, 1000
+
+-- Date: 2023-01-18 18:19
+*/
+INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`) VALUES ('k123','SK II ','화장품');
+INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`) VALUES ('k124','선크림','화장품');
+INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`) VALUES ('k125','바디로션','화장품');
+INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`) VALUES ('k126','비비크림','화장품');
+INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`) VALUES ('k127','아이쉐도우','화장품');
+INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`) VALUES ('k128','휴지','생활용품');
+INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`) VALUES ('k129','비타민','보충제');
+INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`) VALUES ('k130','향수','화장품');
+INSERT INTO `` (`sku_code`,`sku_name`,`sku_kind`) VALUES ('k131','립스틱','화장품');
